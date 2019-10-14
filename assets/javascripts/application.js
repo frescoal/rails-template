6e2155ebf70e0316c0a8c1ae90bd480af88fa749
Quentin Girard <quentin.girard@wavemind.ch>
2019-10-14 16:10:55 +0200

    Add assets

---
+++ assets/javascripts/application.js
@@ -0,0 +1,18 @@
+//= require js.cookie
+//= require jquery
+//= require rails-ujs
+//= require_tree .
+
+
+// Row can be clicked
+$(".table > tbody").on("click", "tr", function (event) {
+  if (event.target.nodeName !== "A" && event.target.nodeName !== "BUTTON") {
+    if (!$(this).hasClass("no-link")) {
+      if (event.ctrlKey) {
+        window.open($(this).attr("href"), "_blank");
+      } else {
+        window.document.location = $(this).attr("href");
+      }
+    }
+  }
+});
